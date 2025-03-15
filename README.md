f6f6a492ee06f9b269ecde761c067c5860d8cf8d
hongdaestreet <window300@naver.com>
2025-03-15 19:30:28 +0900

    03.15 19:30

--- README.md
+++ README.md
@@ -1,4 +1,28 @@
 # Algorithm
 
-- 삼성 B형 대비 문제
+## 삼성 B형 대비 문제
 Solution, UserSolution으로 나누어져있음. UserSolution으로 구현해야 함.
+
+
+## 25.03.15 B형 시험 후기
+### 시간대에 따른 사원 음악청취자 count하는 문제<br><br>
+
+- method<br>
+    - init(int 실행시간) : 초기 method, 실행시간 제공해줌 <br>
+    - add(int mID, int 출근시간, int 퇴근시간) : 사번이 mID인 사원의 출근시간,퇴근시간 <br>
+    - remove(int mID) : 사번이 mID인 사원 삭제<br>
+    - count(int 시작시각) : 시작시각부터 종료시각까지 회사에 있는 사원 수 count<br>
+
+### 나의 풀이
+출근시간, 퇴근시간에 대한 HashMap 각각 사용. <사번,출/퇴근시간> 쌍으로 사용<br>
+테스트 케이스는 다 맞음. But 실행시간이 오래 걸림.<br>
+list<Hashmap>을 정렬하는 방법을 알고 싶었지만 comparator 사용할 줄 몰라 실패 (사실 HashMap<HashMap,HashMap>정렬하고 싶었음)<br>
+결국 iDs라는 list<Integer> 배열 선언 -> 순서 지정해줌 (출근 오름차순,2번째 comparator는 퇴근 내림차순)<br>
+binarysearch 시도했지만 능력 부족으로 실패...
+
+### 후기
+생각보다 쉬웠음.<br>
+테스트케이스 통과는 40분 컷 냄<br>
+근데 시간초과 뜸. 아마도 정렬을 통해 인정되지 않는 출근시간 넘어가면 break해야하는데 안해서 시간초과 뜬 뜻<br>
+**자료구조** 공부하면 충분히 승산있을거라고 보임<br>
+공부해야 할 것 : HashMap, HashSet, Comparator, sort, ArrayList 정렬방법 등
--- 삼성B형대비문제/No3/src/UserSolution.java
+++ 삼성B형대비문제/No3/src/UserSolution.java
@@ -1,107 +1,87 @@
 import java.util.Arrays;
 
 class UserSolution {
-    // 각 id와 능력치가 있는 Player 클래스 생성
-    class Player implements Comparable<Player> {
-        int id;
-        int ability;
-        
-        Player(int id, int ability) {
-            this.id = id;
-            this.ability = ability;
-        }
-        
-        // 성적 기준 내림차순으로 정렬 (높은 성적->상위 리그) + 같으면 낮은 ID가 앞에 오게 함
-        @Override
-        public int compareTo(Player other) {
-            if (this.ability != other.ability) {
-                return other.ability - this.ability; // 성적 내림차순
-            }
-            return this.id - other.id; // 낮은 ID 먼저
-        }
-    }
-    
-    // 리그 수X리그 내 선수 인 leagues 배열 생성
-    private Player[][] leagues;
-    private int numLeagues;     // 리그 수
-    private int playersPerLeague; // 리그 내 선수
-    
-    // 초기설정 method
+	static Player[][] leagues;
+	static int league;
+	static int playerL;
+	
+	class Player implements Comparable<Player> {
+		private int id;
+		private int ability;
+		
+		Player(int id,int ability){
+			this.id = id;
+			this.ability = ability;
+		}
+		
+		
+		@Override
+		public int compareTo(Player other) {
+			if (this.ability != other.ability) {
+				return other.ability - this.ability;  // 높은 애들 먼저
+			}
+			return this.id-other.id;  // 낮은 애들 먼저
+		}
+		
+		
+	}
+	
     void init(int N, int L, int mAbility[]) {
-        numLeagues = L;
-        playersPerLeague = N / L;
-        leagues = new Player[numLeagues][playersPerLeague];
-        
-        int playerIdx = 0;  // 선수 ID
-        // 각 선수마다 Player 클래스 생성함
-        for (int leagueId = 0; leagueId < numLeagues; leagueId++) {
-            for (int pos = 0; pos < playersPerLeague; pos++) {
-                leagues[leagueId][pos] = new Player(playerIdx, mAbility[playerIdx]);
-                playerIdx++; // ID는 초기 순서로 지정
-            }
-        }
+    	int players = N;
+    	league = L;
+    	playerL = N/L;
+    	
+    	leagues = new Player[league][playerL];
+    	int idx = 0;
+    	for(int i=0;i<league;i++) {
+    		for(int j=0;j<playerL;j++) {
+    			leagues[i][j] = new Player(idx,mAbility[idx]);
+    			idx++;
+    		}
+    	}
+    	
+    	sortAll();
+    	
+    	
+    }
+    
+    void sortAll() {
+    	for(int i=0;i<league;i++) {
+    		Arrays.sort(leagues[i]);
+    	}
+    }
 
-        // 성적 순 정렬
-        sortAllLeagues();
-    }
-    
-    // Sort all leagues - each league's players are sorted by ability (higher first)
-    
-    // 각 리그마다 정렬
-    private void sortAllLeagues() {
-        for (int i = 0; i < numLeagues; i++) {
-            Arrays.sort(leagues[i]);
-        }
-    }
-    
-    // 상위팀 꼴등<->하위팀 1등
     int move() {
-        int sumMovedIds = 0;  // 이동한 선수 ID 합 기록용
-        
-        // 2번째 팀 1등부터 바꾸니 index는 1부터 진행
-        for (int i = 1; i < numLeagues; i++) {
-        	// 하위팀 1등
-        	Player bestFromLower = leagues[i][0];
-        	// 상위팀 꼴등
-            Player worstFromUpper = leagues[i-1][playersPerLeague-1];
-            
-            // Swap
-            leagues[i][0] = worstFromUpper;
-            leagues[i-1][playersPerLeague-1] = bestFromLower;
-            
-            // ID 누적합
-            sumMovedIds += bestFromLower.id + worstFromUpper.id;
-        }
-        
-        // 다 끝나면 성적 순 정렬
-        sortAllLeagues();
-        
-        return sumMovedIds;
+    	int sumId = 0;
+    	for(int i=1;i<league;i++) {
+    		Player lowTop = leagues[i][0];
+    		Player highBottom = leagues[i-1][playerL-1];
+    		
+    		leagues[i-1][playerL-1] = lowTop;
+    		leagues[i][0] = highBottom;
+    		
+    		sumId += (lowTop.id + highBottom.id);
+    	}
+    	sortAll();
+    	
+        return sumId;
     }
-    
-    // 상위팀 중간<->하위팀 1등
+
     int trade() {
-        int sumMovedIds = 0;  // 이동한 선수 ID 합 기록용
-        
-        // 2번째 팀 1등부터 바꾸니 index는 1부터 진행
-        for (int i = 1; i < numLeagues; i++) {
-        	// 하위팀 1등
-            Player bestFromLower = leagues[i][0];
-            // 상위팀 중간등수 (index니까 -1해줌)
-            int middleIdx = (playersPerLeague + 1) / 2 - 1;
-            Player middleFromUpper = leagues[i-1][middleIdx];
-            
-            // Swap
-            leagues[i][0] = middleFromUpper;
-            leagues[i-1][middleIdx] = bestFromLower;
-            
-            // ID 누적합
-            sumMovedIds += bestFromLower.id + middleFromUpper.id;
-        }
-        
-        // 다 끝나면 성적 순 정렬
-        sortAllLeagues();
-        
-        return sumMovedIds;
+    	int sumId = 0;
+    	
+    	for(int i=1;i<league;i++) {
+    		Player lowTop = leagues[i][0];
+    		Player highMid = leagues[i-1][(playerL+1)/2-1];
+    		
+    		leagues[i][0] = highMid;
+    		leagues[i-1][(playerL+1)/2-1] = lowTop;
+    		
+    		sumId += (lowTop.id+highMid.id);
+    	}
+    	
+    	sortAll();
+        return sumId;
     }
+
 }
