3656ac2760c01187ad6856afb441be9b0778ee4b
hongdaestreet <window300@naver.com>
2025-03-16 18:13:48 +0900

    03.16 18:13

--- BasicAlgorithm/src/순열조합.java
+++ BasicAlgorithm/src/순열조합.java
@@ -1,4 +1,4 @@
-
+package practice;
 
 import java.util.Arrays;
 
@@ -12,10 +12,10 @@ public class 순열조합 {
 		boolean[] visited = new boolean[5];
 //		System.out.println("순열 (사전식X)");
 //		pmt(0,r);
-//		
+		
 		System.out.println("\n순열 (사전식)");
 		pmt(visited,0,r);
-//
+
 //		System.out.println("\n중복순열");
 //		pmtR(0,r);
 
@@ -36,10 +36,10 @@ public class 순열조합 {
 		}
 		// 재귀 진행
 		for (int i = depth; i < N; i++) {
-			swap(arr, i, depth);
+			swap(i, depth);
 			output[depth] = arr[depth]; // swap하면 swap된 depth 인덱스 사용해야지
 			pmt(depth + 1, r);
-			swap(arr, i, depth); // 다시 원복
+			swap(i, depth); // 다시 원복
 		}
 	}
 
@@ -50,7 +50,7 @@ public class 순열조합 {
 			return;
 		}
 		// 재귀 진행
-		for (int i = 0; i < N; i++) {
+		for (int i = 0; i < N; i++) {  // 순열은 순서있으므로 0부터 다시 다 돌림
 			if (!visited[i]) {
 				visited[i] = true; // 방문 처리
 				output[depth] = arr[i]; // output배열의 depth 인덱스에 arr[i] 할당
@@ -73,32 +73,30 @@ public class 순열조합 {
 		}
 	}
 
-	// 조합 r개
+	// 조합
+	// visited 배열로 방문 처리하면서 배열에 등록함. 그리고 재귀를 실행하는데 다시 선택할 수 있도록 방문X 처리함
 	static void comb(boolean[] visited, int start, int depth, int r) {
 
 		if (depth == r) {
-//			for (int i = 0; i < N; i++) {  // 등록한 요소만 출력
-//				if (visited[i])
-//					System.out.print(arr[i] + " ");
-//			}
-//			System.out.println();
 			print(output);
 			return;
 		}
 
-		for (int i = start; i < N; i++) {
+		for (int i = start; i < N; i++) {  // 조합은 순서 지키므로 이전은 선택X
 			if (!visited[i]) {
-				visited[i] = true;
-				output[depth] = arr[i];
-				comb(visited, i + 1, depth + 1, r); // 다음 차례에 자기 포함X
-				visited[i] = false;
+				visited[i] = true;  // 방문 처리
+				output[depth] = arr[i];  // 등록
+				comb(visited, i + 1, depth + 1, r); // 다음 차례(depth+1)에 자기 포함X (i+1)
+				visited[i] = false; // 다시 방문을 하기 위해 방문X 처리 (백트래킹)
 
 			}
 		}
 
 	}
 
-	// 중복 조합 (start : 다음 대상을 선택할때 탐색 시작할 index)
+	// 중복 조합
+	// 중복으로 담을 것이므로 visited 배열 필요X
+	// (start : 다음 대상을 선택할때 탐색 시작할 index)
 	static void combR(int start, int depth, int r) {
 		if (depth == r) {
 			print(output);
@@ -106,14 +104,14 @@ public class 순열조합 {
 		}
 
 		for (int i = start; i < N; i++) {
-			output[depth] = arr[i];
-			combR(i, depth + 1, r); // 다음 차례에도 자기 자신 포함함
+			output[depth] = arr[i];  // 등록
+			combR(i, depth + 1, r); // 다음 차례(depth+1)에도 자기 자신 포함함(i)
 		}
 
 	}
 
 	// 원소 교환
-	static void swap(int[] arr, int i, int j) {
+	static void swap(int i, int j) {
 		int temp = arr[i];
 		arr[i] = arr[j];
 		arr[j] = temp;
