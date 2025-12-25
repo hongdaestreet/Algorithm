6dd1d508f9c61c059de0fbc6f68e2ae20599ee15
hongdaestreet <102702039+hongdaestreet@users.noreply.github.com>
2025-12-26 03:49:43 +0900

    [251225] [B1] https://www.acmicpc.net/problem/1934

---
+++ 백준/Boj1934.java
@@ -0,0 +1,38 @@
+// https://www.acmicpc.net/problem/1934
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int T = Integer.parseInt(br.readLine());
+
+		for (int t = 0; t < T; t++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			int A = Integer.parseInt(st.nextToken());
+			int B = Integer.parseInt(st.nextToken());
+			long ans = A * B / gcd(A,B);
+			System.out.println(ans);
+		}
+
+
+	static long gcd(long a, long b) {
+		if (a < b) {
+			long temp = a;
+			a = b;
+			b = temp;
+		}
+
+		if (a % b == 0) {
+			return b;
+		} else {
+			return gcd(b,a%b);
+		}
+			
+	}
+
+}
