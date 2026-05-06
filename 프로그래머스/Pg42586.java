d2bbed19806148b4b36cd358bf4885d1e1a0d39c
hongdaestreet <102702039+hongdaestreet@users.noreply.github.com>
2026-05-06 15:22:28 +0900

    Refactor: [260506] [lv2] https://school.programmers.co.kr/learn/courses/30/lessons/42586

--- 프로그래머스/Pg42586.java
+++ 프로그래머스/Pg42586.java
@@ -8,33 +8,26 @@ class Solution {
         
         int N = progresses.length;
         int[] date = new int[N];
-        int cnt = 1;
-        int maxDate = Integer.MIN_VALUE;
+        int cnt = 0;
+        int currentDay = 0;
         List<Integer> list = new ArrayList<>();
         for(int i=0;i<N;i++) {
-            // int day = (100 - progresses[i] + speeds[i] - 1) / speeds[i];  // 정수 올림 공식 이용
-            int last = 100 - progresses[i];
-            date[i] = last/speeds[i];
-            if (last%speeds[i] != 0) {
-                date[i]++;
-            }
+            int day = (100 - progresses[i] + speeds[i] - 1) / speeds[i];  // 정수 올림 공식 이용
             if (i == 0) {
-                maxDate = date[i];
+                currentDay = day;
+                cnt++;
+            } else if (day <= currentDay) {
+                cnt++;
             } else {
-                if (maxDate<date[i]) {
-                    maxDate = date[i];
-                    list.add(cnt);
-                    cnt = 1;
-                } else {
-                    cnt++;
-                }
-                
-                if (i == N-1) {
-                    list.add(cnt);
-                }
+                list.add(cnt);
+                currentDay = day;
+                cnt = 1;
             }
             
         }
+        
+        // 마지막꺼 넣기
+        list.add(cnt);
         
         int[] answer = new int[list.size()];
         
