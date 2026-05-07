4a0645e374e97ce70e58274b82b8ea9d4a72a64d
hongdaestreet <102702039+hongdaestreet@users.noreply.github.com>
2026-05-07 21:28:25 +0900

    Refactor: [260507] [lv2] https://school.programmers.co.kr/learn/courses/30/lessons/12909

--- 프로그래머스/Pg12909.java
+++ 프로그래머스/Pg12909.java
@@ -1,6 +1,8 @@
 // https://school.programmers.co.kr/learn/courses/30/lessons/12909
-// 7M / 효율성 실패
-// Stack 쓰면 시간 초과?
+// 7M / 효율성 통과 / AI(Support)
+// Stack<Character>는 괄호 하나를 만날 때마다 Character 객체를 다루고, 내부 배열에 넣고 빼는 작업을 함.
+// 반면 int count는 숫자 하나만 증가/감소하면 끝임.
+// java.util.Stack은 내부적으로 동기화 비용도 있음. push, pop 같은 메서드가 Vector 기반이라 상대적으로 느림
 
 import java.util.*;
 
@@ -9,21 +11,21 @@ class Solution {
         boolean answer = true;
 
         int N = s.length();
-        Stack<Character> st = new Stack<>();
+        int count = 0;
         
         for(int i=0;i<N;i++) {
             if (s.charAt(i) == '(') {
-                st.push('(');
+                count++;
             } else {
-                if (st.isEmpty()) {
+                count--;
+                if (count<0) {
                     answer = false;
                     break;                    
-                } else {
-                    st.pop();
                 }
             }
         }
-        if (!st.isEmpty()) {
+        
+        if (count != 0) {
             answer = false;
         }
 
