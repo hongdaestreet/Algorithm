879049be019e5a178bf1b0a200105eb21bfe2f68
hongdaestreet <102702039+hongdaestreet@users.noreply.github.com>
2026-05-08 13:44:50 +0900

    [260508] [RE] [lv2] https://school.programmers.co.kr/learn/courses/30/lessons/42577

--- 프로그래머스/Pg42577.java
+++ 프로그래머스/Pg42577.java
@@ -1,6 +1,6 @@
 // https://school.programmers.co.kr/learn/courses/30/lessons/42577#
-// 30M
-// 접두어 == 문장 앞 단어
+// 39M / AI(Support)
+// 1.접두어 == 문장 앞 단어 2.일반 정렬->사전 순 정렬 3.사전 순으로 정렬하면 바로 뒤만 확인하면 됨
 
 
 import java.util.*;
@@ -8,32 +8,24 @@ import java.util.*;
 class Solution {
     public boolean solution(String[] phone_book) {
         
-        Arrays.sort(phone_book,(a,b) ->{
-            return a.length() - b.length();
-        });
+        Arrays.sort(phone_book);  // 사전순 정렬 ex. "12", "123", "1234"
         
         int N = phone_book.length;
         
         for(int i=0;i<N-1;i++) {
             String curr = phone_book[i];
-            for(int j=i+1;j<N;j++) {
-                for(int k=0;k<=phone_book[j].length() - curr.length();k++) {
-                    boolean same = true;
-                    for(int w = 0; w < curr.length();w++) {
-                        if (curr.charAt(w) != phone_book[j].charAt(w+k)) {
-                            same = false;
-                            break;
-                        }
-                    }
-                    if (same) {
-                        return false;
-                    }
-                    
-                    // if (curr.equals(phone_book[j].substring(k,k+curr.length()))) {
-                    //     return false;
-                    // }
-                }
+            String next = phone_book[i+1]; // 사전 순 정렬이니까 바로 뒤만 확인하면 됨
+            boolean same = true;
+            for(int j = 0; j < curr.length();j++) {
+                if (curr.charAt(j) != next.charAt(j)) {
+                    same = false;
+                    break;
+                }            
             }
+            if (same) {
+                return false;
+            }            
+
         }
         
         
